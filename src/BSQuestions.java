26358d2571312526aa4eb6b9530899d18a7ce055
Varun <varunrawat343@gmail.com>
2023-07-25 14:39:38 +0530

    Studied Binary Search

---
+++ src/BSQuestions.java
@@ -0,0 +1,74 @@
+public class BSQuestions {
+    public static void main(String[] args) {
+
+
+
+//        Q.3
+
+
+
+//        Q.2
+//        int[] arr = {2,3,5,9,14,16,18};
+//        int target = 6;
+//        System.out.println(floor(arr,target));
+
+
+//        Q.1
+//        int[] arr = {2,3,5,9,14,16,18};
+//        int target = 12;
+//        System.out.println(ceiling(arr,target));
+    }
+
+
+    /*
+    Above is main function
+    ----------------------
+    Below are functions
+     */
+
+//    Q.3
+
+
+
+
+//    Q.2 Find floor in array for a target number
+//    static int floor(int[] arr, int target){
+//        int s = 0;
+//        int e = arr.length-1;
+//
+//        while(s<=e){
+//            int mid = s + (e-s)/2;
+//            if (arr[mid] == target){
+//                return arr[mid];
+//            }
+//            if (arr[mid] > target){
+//                e = mid-1;
+//            }
+//            if (arr[mid] < target){
+//                s = mid+1;
+//            }
+//        }
+//        return arr[e];
+//    }
+
+
+//    Q.1 Find ceiling in the array for a target number.
+//     static int ceiling(int[] arr, int target){
+////        int s = 0;
+////        int e = arr.length-1;
+////
+////        while(s<=e){
+////            int mid = s + (e-s)/2;
+////            if (arr[mid] == target){
+////                return arr[mid];
+////            }
+////            if (arr[mid] > target){
+////                e = mid-1;
+////            }
+////            if (arr[mid] < target){
+////                s = mid+1;
+////            }
+////        }
+////        return arr[s];
+////    }
+}
---
+++ src/InfiniteArray.java
@@ -0,0 +1,46 @@
+// https://www.geeksforgeeks.org/find-position-element-sorted-array-infinite-numbers/
+public class InfiniteArray {
+    public static void main(String[] args) {
+        int[] arr = {3, 5, 7, 9, 10, 90,
+                100, 130, 140, 160, 170};
+        int target = 100;
+        System.out.println(infiniteArray(arr, target, 0,1));
+    }
+
+    static int infiniteArray(int[] arr, int target,int start, int end ){
+        // condition for the target to lie in the range
+        if(end - start < 0) {
+            return -1;
+        }
+        try {
+            while(target > arr[end]) {
+                int oldS = start;
+                start = end+1;
+                end += (end+1-oldS)*2;
+            }
+        }catch(ArrayIndexOutOfBoundsException e) {
+            end = end - (end-start)/2 - 1;
+            return infiniteArray(arr, target , start, end);
+        }
+        System.out.println("s :"+start +" end: "+end);
+        return binarySearch(arr, target, start, end);
+
+    }
+    static int binarySearch(int[] arr, int target, int start, int end) {
+        while(start <= end) {
+            // find the middle element
+//            int mid = (start + end) / 2; // might be possible that (start + end) exceeds the range of int in java
+            int mid = start + (end - start) / 2;
+
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                // ans found
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
